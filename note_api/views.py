49db75300465c04b8bb4c5f2488599cc9c29a176
JayJaySea <xkalibur@wp.pl>
2023-03-18 13:03:18 +0100

    Add get and post endpoint for notes
    Endpoints are accessible only for authorized users

--- note_api/views.py
+++ note_api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from .models import Note
+from .serializers import NoteSerializer
+
+class NoteListApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        notes = Note.objects.filter(user = request.user.id)
+        serializer = NoteSerializer(notes, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'text': request.data.get('text'),
+            'priority': request.data.get('priority'),
+            'user': request.user.id
+        }
+
+        serializer = NoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
