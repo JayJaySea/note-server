d82b33bb4aadfaf13e727d10d8ba8b9228c1aa83
JayJaySea <xkalibur@wp.pl>
2023-03-18 11:48:35 +0100

    Add Note model with serializer

--- note_api/models.py
+++ note_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    text = models.CharField(max_length = 500)
+    timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
+    priority = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.text
